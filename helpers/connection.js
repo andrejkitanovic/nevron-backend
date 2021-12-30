38d13eff1749153f28228001c401a3dba158e660
shindiristudio-git <64252193+shindiristudio-git@users.noreply.github.com>
2021-12-30 15:55:19 +0100

    host added

--- helpers/connection.js
+++ helpers/connection.js
@@ -1,15 +1,14 @@
 const { Sequelize } = require("sequelize");
-const mongoose = require("mongoose");
 const http = require("http");
 
 global.sequelize = new Sequelize("nevron", "nevron", "centos", {
   host: "212.103.140.194",
   dialect: "mariadb",
 });
-global.sequelize.sync()
+global.sequelize.sync();
 
 global.server = http.createServer(global.app);
 
-server.listen(global.port, () => {
+server.listen(global.port, global.host, () => {
   console.log(`listening on ${global.port}`);
 });
